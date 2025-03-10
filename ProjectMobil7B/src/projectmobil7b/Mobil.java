86cdd7d96dda366bdadbabf8db94617bf86c3854
Dimas Putra <112814587+Dimasputra2530@users.noreply.github.com>
2025-03-10 11:24:51 +0700

    Add files via upload

---
+++ ProjectMobil7B/src/projectmobil7b/Mobil.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectmobil7b;
+
+/**
+ *
+ * @author Dimas Putra Nugroho 2311103054
+ */
+public class Mobil {
+    String warna = "Merah";
+    String merk = "Mercy";
+    
+    
+    void maju(){
+        System.out.println("Mobil "+merk+" warna "+warna+" maju");
+    }
+    
+    void mundur(){
+        System.out.println("Mobil "+merk+" warna "+warna+" mundur");
+    }
+}
---
+++ ProjectMobil7B/src/projectmobil7b/ProjectMobil7B.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectmobil7b;
+
+/**
+ *
+ * @author Dimas Putra Nugroho 2311103054
+ */
+public class ProjectMobil7B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mobil MobilSaya = new Mobil();
+        MobilSaya.maju();
+        MobilSaya.mundur();
+    }
+    
+}
